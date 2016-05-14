bc0e47022fb9f3540d4e03dc94a51b60694125a6
Cal Langdon <Cal Langdon>
2016-05-13 21:43:44 -0400

    Add recursive function

--- new.java
+++ new.java
@@ -1,6 +1,13 @@
 public class JLee {
     public static void main(String[] args) {
-        System.out.print("Initial Add\n");
-        System.out.print("I love indians");
+        //Factorial 5!
+        System.out.print("The Factorial of 5 is " + fact(5));
+        System.out.println("\n");
+    }
+    
+    //learn recursion 
+    public static int fact(int n)
+    {
+         return n * fact(n-1); 
     }
 }
