7e6949b126d6c6a8c2bd350ad4949b5772633a74
Jordan Lee <jslee14@outlook.com>
2017-03-01 15:30:18 -0500

    #BOOM

--- new.java
+++ new.java
@@ -3,11 +3,12 @@ public class JLee {
         //Factorial 5!
         System.out.print("The Factorial of 5 is " + fact(5));
         System.out.println("\n");
+        System.out.println("How to bamboozle");
     }
-    
-    //learn recursion 
+
+    //learn recursion
     public static int fact(int n)
     {
-         return n * fact(n-1); 
+         return n * fact(n-1);
     }
 }
